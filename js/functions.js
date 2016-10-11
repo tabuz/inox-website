432056ebe3cd59b42dc15ba62a98a4507341cf5d
tabuz <zelek.hubert@gmail.com>
2016-10-12 00:18:11 +0100

    Header and slider++

---
+++ js/functions.js
@@ -0,0 +1,35 @@
+$(function(){
+  slider();
+});
+
+function slider(){
+
+  var i = 1;
+  $('.slider-control-next, .slider-control-prev').click(function(){
+
+    var $this = $(this);
+
+    if($this.hasClass('slider-control-next')){
+      i++;
+
+      if( i<4 ){
+        $('.slider-picture img').attr('src', '../img/hero'+i+'.png');
+        console.log(i);
+      }else{
+        i = 1;
+        $('.slider-picture img').attr('src', '../img/hero'+i+'.png');
+        console.log(i);
+      }
+    }else{
+      i--;
+      console.log(i);
+      if( i<1 ){
+        i = 3;
+        $('.slider-picture img').attr('src', '../img/hero'+i+'.png');
+      }else{
+        $('.slider-picture img').attr('src', '../img/hero'+i+'.png');
+      }
+    }
+
+  });
+}
