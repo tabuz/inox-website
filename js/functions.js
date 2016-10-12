c268facaee773f647d97d0ada784efeec92a784c
tabuz <zelek.hubert@gmail.com>
2016-10-12 23:31:00 +0100

    refactoring

--- js/functions.js
+++ js/functions.js
@@ -6,6 +6,11 @@ $(document).ready(function(){
     nav:true,
     autoplay:true,
     autoplayTimeout:3000,
+    responsive:{
+                0:{items:1},
+                600:{items:2},
+                1000:{items:4}
+                }
 
   });
 });
