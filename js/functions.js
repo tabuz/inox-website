f205c451b88db99cfa7d0128b2e20e4a9e998bdc
tabuz <zelek.hubert@gmail.com>
2016-10-13 21:48:36 +0100

    Section blog++

--- js/functions.js
+++ js/functions.js
@@ -14,14 +14,14 @@ $(document).ready(function(){
 
   });
   $('#owl-service').owlCarousel({
-    items:3,
+    items:4,
     loop:true,
     nav:true,
     autoplay:true,
-    autoplayTimeout:3000,
+    autoplayTimeout:7000,
     responsive:{
                 0:{items:1},
-                600:{items:1},
+                600:{items:2},
                 1000:{items:1}
                 }
 
